a4cc6fbff6b2b487f9b69199db7d168a4d271d72
Jon Surrell <jon.surrell@automattic.com>
2018-07-11 10:12:17 +0200

    Remove fully-enabled "onboarding-checklist" flag

--- client/my-sites/stats/site.jsx
+++ client/my-sites/stats/site.jsx
@@ -151,7 +151,7 @@ class StatsSite extends Component {
 					slug={ slug }
 				/>
 				<div id="my-stats-content">
-					{ config.isEnabled( 'onboarding-checklist' ) && <ChecklistBanner siteId={ siteId } /> }
+					<ChecklistBanner siteId={ siteId } />
 					{ siteId && (
 						<GoogleMyBusinessStatsNudge
 							siteSlug={ slug }
